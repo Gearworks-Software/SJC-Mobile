9277e9c00bf39b059bc8ff74babca24b756585af
Javian <javianluc@gmail.com>
2025-04-26 19:39:41 -0600

    Update home-view-model.ts

--- frontend/app/home-view-model.ts
+++ frontend/app/home-view-model.ts
@@ -1,12 +1,21 @@
 import { Observable } from "@nativescript/core";
+import { Frame } from '@nativescript/core';
+import { EventData } from '@nativescript/core';
+
+export class HomeViewModel extends observable{
+
+  constructor() {
+    super();
+  }
+
+public onCounselingTap(args: EventData) {
+        const frame = Frame.topmost();
+        frame.navigate("counseling");
+    }
+}
 
 export function onNavigatingTo(args) {
     const page = args.object;
     page.bindingContext = new HomeViewModel();
 }
 
-class HomeViewModel extends Observable {
-    constructor() {
-        super();
-    }
-}
