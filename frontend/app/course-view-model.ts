85d3dba77bd7436f7f43f3189e536a1c67a81e19
Shinj1n <allenkieran29@gmail.com>
2025-04-30 00:05:20 -0600

    Update: Add assignments, grades, and average

--- frontend/app/course-view-model.ts
+++ frontend/app/course-view-model.ts
@@ -3,9 +3,16 @@ import { Observable } from '@nativescript/core';
 interface Course {
     course_id: string;
     title: string;
+    section: string;
     description: string;
-    // imageUrl: string;
     studentsEnrolled: number;
+    assignments: Array<Assignment>;
+}
+
+interface Assignment{
+    name: string;
+    grade: number;
+    is_completed: boolean;
 }
 
 export class CourseViewModel extends Observable {
@@ -18,12 +25,28 @@ export class CourseViewModel extends Observable {
 
     private loadCourses() {
         this.courses = [
-            { course_id: '0', title: 'ENG 110', description: 'College Composition I', studentsEnrolled: 25},
-            { course_id: '1', title: 'MTH 103', description: 'Intermediate Algebra', studentsEnrolled: 30},
-            { course_id: '2', title: 'PHIL 101', description: 'Philosophy of Ethics', studentsEnrolled: 18},
-            { course_id: '3', title: 'SOC 121', description: 'Introduction to Research Methods', studentsEnrolled: 22},
-            { course_id: '4', title: 'CIS 125', description: 'Principles of Programming I', studentsEnrolled: 20},
-            { course_id: '5', title: 'COLL 101', description: 'College Seminar', studentsEnrolled: 30}
+            { course_id: '0', title: 'ENG 110', section: '1', description: 'College Composition I', studentsEnrolled: 25, assignments: [{name:'Homework 1', grade: 69, is_completed: true}, {name:'Homework 2', grade: 86, is_completed: true}]},
+            { course_id: '1', title: 'MTH 103', section: '3', description: 'Intermediate Algebra', studentsEnrolled: 30, assignments: [{name:'Homework 1', grade: 95, is_completed: true}, {name:'Homework 2', grade: 70, is_completed: true}]},
+            { course_id: '2', title: 'PHIL 101', section: '11', description: 'Philosophy of Ethics', studentsEnrolled:18, assignments: [{name:'Homework 1', grade: 0, is_completed: false}, {name:'Homework 2', grade: 95, is_completed: true}]},
+            { course_id: '3', title: 'SOC 121', section: '5', description: 'Introduction to Research Methods', studentsEnrolled: 22, assignments: [{name:'Homework 1', grade: 76, is_completed: true}, {name:'Homework 2', grade: 0, is_completed: false}]},
+            { course_id: '4', title: 'CIS 125', section: '7', description: 'Principles of Programming I', studentsEnrolled: 20, assignments: [{name:'Homework 1', grade: 0, is_completed: false}, {name:'Homework 2', grade: 0, is_completed: false}]},
+            { course_id: '5', title: 'COLL 101', section: '2', description: 'College Seminar', studentsEnrolled: 30, assignments: [{name:'Homework 1', grade: 74, is_completed: true}, {name:'Homework 2', grade: 70, is_completed: true}]}
         ];
+    }
+
+    public calculateAverage(){
+        let total: 0;
+        let count: 0;
+
+        for(const course of this.courses){
+            for(const assignment of course.assignments){
+                if(assignment.is_completed)
+                {
+                    total+= assignment.grade;
+                    count++;
+                }
+            }
+        }
+        return total;
     }
 }
